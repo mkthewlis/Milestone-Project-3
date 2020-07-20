e6fc4f71a046bb3e87399a5de0fc9b00fe8cf7dc
mkthewlis <mkthewlis@gmail.com>
2020-07-20 15:26:50 +0000

    docs: added test function to check app.py set up correctly

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello World! Here I come!'
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+    port=int(os.environ.get('PORT')),
+    debug=True)
+

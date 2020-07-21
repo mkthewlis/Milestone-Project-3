2b33b6b92b9c60db5a7cd06bf72a4b971a6a310e
mkthewlis <mkthewlis@gmail.com>
2020-07-21 09:44:19 +0000

    docs: added .gitignore and env.py files to prepare the link between my mongoDB and heroku app

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+
+app.config["MONGO_BDNAME"] = 'moveon_database'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 
 @app.route('/')

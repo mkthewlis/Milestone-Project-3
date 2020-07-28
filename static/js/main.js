f08daf659beac4f9b51ad12aae32f480ca89d51b
mkthewlis <mkthewlis@gmail.com>
2020-07-28 14:25:47 +0000

    feat: used clipboard.js to successfully allow users to copy website url from footer

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,21 @@
-  
-//Only runs function when page is ready
+let clipboard = new Clipboard('.copy-icon');
+
 $(document).ready(function(){
 
-    //Inspired by source code found on here: https://www.journaldev.com/5446/how-to-create-scroll-to-top-animation-in-jquery
+    //Scrolltop inspired by source code found on here: https://www.journaldev.com/5446/how-to-create-scroll-to-top-animation-in-jquery
     $("a[href='#container']").click(function () {
         $("html, body").animate({ scrollTop: 0 }, 700);
-        return false;
+            return false;
+        });
+
+    // Notifies user the url has successfully been copied
+    clipboard.on('success', function(e) {
+        $('.copy-icon').append('<p class="copy-success">Copied!</p>');
     });
 
-    $("a[href='#container']").click(function () {
-    //  Get the text field
-
-    console.log("Hejsan!")
-
+    // Triggers an alert so the user knows to copy the url themselves
+    clipboard.on('error', function(e) {
+        alert("Oops, it looks like this function isn't supported on your browser! Manually copy this url for the same effect: http://ms3-move-on.herokuapp.com/index");
     });
 
 });

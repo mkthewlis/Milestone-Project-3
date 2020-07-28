1a8d7aa85fec75960c40972e96448776d8e8b90c
mkthewlis <mkthewlis@gmail.com>
2020-07-28 11:19:04 +0000

    feat: added javascript function to allow the user scroll to the top of the page from the footer

---
+++ static/js/main.js
@@ -0,0 +1,23 @@
+  
+//Only runs function when page is ready
+$(document).ready(function(){
+
+    //Inspired by source code found on here: https://www.journaldev.com/5446/how-to-create-scroll-to-top-animation-in-jquery
+    $("a[href='#container']").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 700);
+        return false;
+    });
+
+});
+
+
+
+
+    //$('.expand-btn').click(function(){
+        //$(".btn-container").find((".expand-btn").not($(this)).addClass(".collapse"));
+    //});
+
+
+
+
+
--- static/js/overview.js
+++
@@ -1,7 +0,0 @@
-  
-//Only runs function when page is ready
-$(document).ready(function(){
-    //$('.expand-btn').click(function(){
-        //$(".btn-container").find((".expand-btn").not($(this)).addClass(".collapse"));
-    //});
-});

7ce5ee7e14f5e7d75142f95d31fdb41960cb976e
mkthewlis <mkthewlis@gmail.com>
2020-07-29 16:53:52 +0000

    feat: added color change feature to navbar scroll with jQuery

--- static/js/main.js
+++ static/js/main.js
@@ -2,20 +2,28 @@ let clipboard = new Clipboard('.copy-icon');
 
 $(document).ready(function(){
 
-    //Scrolltop inspired by source code found on here: https://www.journaldev.com/5446/how-to-create-scroll-to-top-animation-in-jquery
+    //Allows user to smooth scroll to top from footer, inspired by source code found on here: https://www.journaldev.com/5446/how-to-create-scroll-to-top-animation-in-jquery
     $("a[href='#container']").click(function () {
         $("html, body").animate({ scrollTop: 0 }, 700);
             return false;
         });
 
-    // Notifies user the url has successfully been copied
+    // Success message when user has copied URL from footer
     clipboard.on('success', function(e) {
         $('.copy-icon').append('<p class="copy-success">Copied!</p>');
     });
 
-    // Triggers an alert so the user knows to copy the url themselves
+    // If copy to clipboard function is unavailable, an error is triggered here
     clipboard.on('error', function(e) {
         alert("Oops, it looks like this function isn't supported on your browser! Manually copy this url for the same effect: http://ms3-move-on.herokuapp.com/index");
+    });
+
+    // Adds background color to navbar on scroll down, inspired by source code found on here: https://stackoverflow.com/questions/23706003/changing-nav-bar-color-after-scrolling
+    $(function () {
+        $(document).scroll(function () {
+        var $nav = $(".fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        });
     });
 
 });

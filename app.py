3dc828c3e5c5aefa33bde97cb8b27c86ec665967
mkthewlis <mkthewlis@gmail.com>
2020-08-12 10:51:49 +0000

    feat: set 'debug=False' ready to complete project

--- app.py
+++ app.py
@@ -377,4 +377,4 @@ if __name__ == '__main__':
     app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
